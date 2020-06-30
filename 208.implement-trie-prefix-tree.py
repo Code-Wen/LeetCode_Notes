4d8bceebebc99dec04c0b9c660dc34a1bf6824d5
SoloZoro <chenxu.wen.math@gmail.com>
2020-06-30 08:30:31 -0700

    LC+3

---
+++ 208.implement-trie-prefix-tree.py
@@ -0,0 +1,93 @@
+#
+# @lc app=leetcode id=208 lang=python3
+#
+# [208] Implement Trie (Prefix Tree)
+#
+# https://leetcode.com/problems/implement-trie-prefix-tree/description/
+#
+# algorithms
+# Medium (48.03%)
+# Likes:    3108
+# Dislikes: 49
+# Total Accepted:    316.5K
+# Total Submissions: 654.6K
+# Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
+  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+#
+# Implement a trie with insert, search, and startsWith methods.
+# 
+# Example:
+# 
+# 
+# Trie trie = new Trie();
+# 
+# trie.insert("apple");
+# trie.search("apple");   // returns true
+# trie.search("app");     // returns false
+# trie.startsWith("app"); // returns true
+# trie.insert("app");   
+# trie.search("app");     // returns true
+# 
+# 
+# Note:
+# 
+# 
+# You may assume that all inputs are consist of lowercase letters a-z.
+# All inputs are guaranteed to be non-empty strings.
+# 
+# 
+#
+
+# @lc code=start
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for w in prefix:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
+
---
+++ 212.word-search-ii.py
@@ -0,0 +1,108 @@
+#
+# @lc app=leetcode id=212 lang=python3
+#
+# [212] Word Search II
+#
+# https://leetcode.com/problems/word-search-ii/description/
+#
+# algorithms
+# Hard (33.26%)
+# Likes:    2360
+# Dislikes: 105
+# Total Accepted:    199.5K
+# Total Submissions: 591.3K
+# Testcase Example:  '[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]\n' +
+  '["oath","pea","eat","rain"]'
+#
+# Given a 2D board and a list of words from the dictionary, find all words in
+# the board.
+# 
+# Each word must be constructed from letters of sequentially adjacent cell,
+# where "adjacent" cells are those horizontally or vertically neighboring. The
+# same letter cell may not be used more than once in a word.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: 
+# board = [
+# ⁠ ['o','a','a','n'],
+# ⁠ ['e','t','a','e'],
+# ⁠ ['i','h','k','r'],
+# ⁠ ['i','f','l','v']
+# ]
+# words = ["oath","pea","eat","rain"]
+# 
+# Output: ["eat","oath"]
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# All inputs are consist of lowercase letters a-z.
+# The values of words are distinct.
+# 
+# 
+#
+
+# @lc code=start
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+    
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+    
+    def search(self, word):
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.isWord
+    
+class Solution(object):
+    def findWords(self, board, words):
+        res = []
+        trie = Trie()
+        node = trie.root
+        for w in words:
+            trie.insert(w)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                self.dfs(board, node, i, j, "", res)
+        return res
+    
+    def dfs(self, board, node, i, j, path, res):
+        if node.isWord:
+            res.append(path)
+            node.isWord = False
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return 
+        tmp = board[i][j]
+        node = node.children.get(tmp)
+        if not node:
+            return 
+        board[i][j] = "#"
+        self.dfs(board, node, i+1, j, path+tmp, res)
+        self.dfs(board, node, i-1, j, path+tmp, res)
+        self.dfs(board, node, i, j-1, path+tmp, res)
+        self.dfs(board, node, i, j+1, path+tmp, res)
+        board[i][j] = tmp
+
+
+
+# @lc code=end
+
